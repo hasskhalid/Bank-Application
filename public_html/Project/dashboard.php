b7f56f18ee5585a7b6c0e17ed1d2ce068cde191e
Hassan Khalid <hassan@hassans-mbp.myfiosgateway.com>
2021-12-02 18:25:28 -0500

    created dashboard page

---
+++ public_html/Project/dashboard.php
@@ -0,0 +1,23 @@
+<?php
+require(__DIR__ . "/../../partials/nav.php");
+?>
+<h1>Dashboard</h1>
+<?php
+
+if (is_logged_in(true)) {
+    echo "Hello, " . get_username();
+    //comment this out if you don't want to see the session variables  
+}
+?>
+
+<link rel="stylesheet" href="styles.css">
+<ul>
+    <li class="dash-link"><a href="#">Create Account</a></li>
+    <li class="dash-link"><a href="#">My Accounts</a></li>
+    <li class="dash-link"><a href="#">Deposit/Withdraw</a></li>
+    <li class="dash-link"><a href="#">Transaction History</a></li>
+    <li class="dash-link"><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
+</ul>
+<?php
+require(__DIR__ . "/../../partials/flash.php");
+?>
