dff76418a53485f990be7b7d7394715c8a64f061
Hassan Khalid <hassan@hassans-mbp.myfiosgateway.com>
2021-12-02 16:10:27 -0500

    created transactions table

--- public_html/Project/sql/007_insert_sys_account.sql
+++ public_html/Project/sql/007_insert_sys_account.sql
@@ -1,5 +1,5 @@
-INSERT INTO BGD_Accounts )id, account, user_id)
-VALUES )(-1, "dbg_sys_acct", -1);
+INSERT INTO Accounts (id, account, user_id)
+VALUES(-1, "sys_acc", null);
 
 INSERT INTO Users (id, email,username, password)
 VALUES(-1, "", "system", "");
